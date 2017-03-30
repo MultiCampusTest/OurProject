b279ac5b42de9aca0927805af52017249337361c
student <student@M11024>
2017-03-30 16:30:00 +0900

    constant

--- Jvb_tm/src/java/commons/Constant.java
+++ Jvb_tm/src/java/commons/Constant.java
@@ -2,71 +2,87 @@ package java.commons;
 
 public class Constant {
 	
-	//Board
-	public static String B_IDX="b_idx";
-	public static String B_CODE="b_code";
-	public static String B_LOC_CATEGORY="b_loc_category";
-	public static String B_SUB_CATEGORY="b_sub_category";
-	public static String B_TITLE="b_title";
-	public static String B_START_DATE="b_start_date";
-	public static String B_END_DATE="b_end_date";
-	public static String USERID="userid";
-	public static String B_DATE="b_date";
+	class Member{
+		public static final String U_IDX="u_idx";
+		public static final String USERID="userid";
+		public static final String PWD="pwd";
+		public static final String NAME="name";
+		public static final String EMAIL="email";
+		public static final String BIRTHDAY="birthday";
+		public static final String NATIONALITY="nationality";
+		public static final String GENDER="gender";
+		public static final String INTRODUCE="introduce";
+		public static final String U_DATE="u_date";
+	}
 	
 	
-	//Comments
-	public static String CM_IDX="cm_idx";
-	public static String CM_CONTENTS="cm_contents";
-	public static String CM_DATE="cm_date";
+	class Image{
+		public static final String IMG_IDX="img_idx";
+		public static final String IMG_PHY_NAME="img_phy_name";
+		public static final String IMG_ORI_NAME="img_ori_name";
+		public static final String IMG_CODE="img_code";
+		public static final String IMG_PATH="img_path";
+	}
+	
+	class Board{
+		public static final String B_IDX="b_idx";
+		public static final String B_CODE="b_code";
+		public static final String B_LOC_CATEGORY="b_loc_category";
+		public static final String B_SUB_CATEGORY="b_sub_category";
+		public static final String B_TITLE="b_title";
+		public static final String B_START_DATE="b_start_date";
+		public static final String B_END_DATE="b_end_date";
+		public static final String USERID="userid";
+		public static final String B_DATE="b_date";
+	}
 	
 	
-	//Member
-	public static String U_IDX="u_idx";
-	public static String PWD="pwd";
-	public static String NAME="name";
-	public static String EMAIL="email";
-	public static String BIRTHDAY="birthday";
-	public static String NATIONALITY="nationality";
-	public static String GENDER="gender";
-	public static String INTRODUCE="introduce";
-	public static String U_DATE="u_date";
+	class Contents{
+		
+		public static final String CT_IDX="ct_idx";
+		public static final String B_IDX="b_idx";
+		public static final String CT_NUM="ct_num";
+		public static final String CT_CONTENTS="ct_contents";
+	}
 	
 	
-	//Image
-	public static String IMG_IDX="img_idx";
-	public static String IMG_PHY_NAME="img_phy_name";
-	public static String IMG_ORI_NAME="img_ori_name";
-	public static String IMG_CODE="img_code";
-	public static String IMG_PATH="img_path";
+	class MapPosition{
+		public static final String MAP_IDX="map_idx";
+		public static final String B_IDX="b_idx";
+		public static final String MAP_MARKER="map_marker";
+		public static final String MAP_LAT_POSITION="map_lat_position";
+		public static final String MAP_LON_POSITION="map_lon_position";
+	}
 	
 	
-	//Contents
-	public static String CT_IDX="ct_idx";
-	public static String CT_NUM="ct_num";
-	public static String CT_CONTENTS="ct_contents";
+	class Comments{
+		public static final String CM_IDX="cm_idx";
+		public static final String B_IDX="b_idx";
+		public static final String USERID="userid";
+		public static final String CM_CONTENTS="cm_contents";
+		public static final String CM_DATE="cm_date";		
+	}
 	
 	
-	//MapPosition
-	public static String MAP_IDX="map_idx";
-	public static String MAP_MARKER="map_marker";
-	public static String MAP_LAT_POSITION="map_lat_position";
-	public static String MAP_LON_POSITION="map_lon_position";
+	
+	class Message{
+		public static final String MSG_IDX="msg_idx";
+		public static final String MSG_SEND_USERID="msg_send_userid";
+		public static final String MSG_RECEIVE_USERID="msg_receive_userid";
+		public static final String MSG_CONTENTS="msg_contents";
+		public static final String MSG_DATE="msg_date";
+	}
 	
 	
-	//Message
-	public static String MSG_IDX="msg_idx";
-	public static String MSG_SEND_USERID="msg_send_userid";
-	public static String MSG_RECEIVE_USERID="msg_receive_userid";
-	public static String MSG_CONTENTS="msg_contents";
-	public static String MSG_DATE="msg_date";
-	
-	
-	//Matching
-	public static String MCH_IDX="mch_idx";
-	public static String MCH_T_USERID="mch_t_userid";
-	public static String MCH_G_USERID="mch_g_userid";
-	public static String MCH_CODE="mch_code";
-	public static String MCH_DATE="mch_date";
+	class Matching{
+		public static final String MCH_IDX="mch_idx";
+		public static final String MCH_T_USERID="mch_t_userid";
+		public static final String MCH_G_USERID="mch_g_userid";
+		public static final String USERID="userid";
+		public static final String MCH_CODE="mch_code";
+		public static final String MCH_DATE="mch_date";
+	}
+
 	
 
 }

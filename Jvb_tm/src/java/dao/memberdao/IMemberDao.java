39161c50cf227fafd5843ea745df50904c61059d
jiamorena <seokhn09@gmail.com>
2017-03-30 20:05:44 +0900

    test

--- Jvb_tm/src/java/dao/memberdao/IMemberDao.java
+++ Jvb_tm/src/java/dao/memberdao/IMemberDao.java
@@ -8,9 +8,5 @@ public interface IMemberDao {
 	public HashMap<String, Object> getId(HashMap<String, Object> params);
 	public HashMap<String, Object> getPwd(HashMap<String, Object> params);
 	public HashMap<String, Object> idCheck(HashMap<String, Object> params);
-	public int insertMember(HashMap<String, Object> params);
-	public HashMap<String, Object> selectOne(String userid);
-	public int updateMember(HashMap<String, Object> params);
-	public int deleteMember(String userid);
-	public HashMap<String, Object> deletePwdCheck(String userid);
+
 }

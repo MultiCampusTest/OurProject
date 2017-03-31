0ab794c6df92e2e37c731b46fef929cc7af5abae
jiamorena <seokhn09@gmail.com>
2017-03-31 17:17:03 +0900

    up

--- Jvb_tm/src/java/dao/memberdao/IMemberDao.java
+++ Jvb_tm/src/java/dao/memberdao/IMemberDao.java
@@ -7,11 +7,11 @@ public interface IMemberDao {
 	public HashMap<String, Object> loginCheck(HashMap<String, Object> params);
 	public HashMap<String, Object> getId(HashMap<String, Object> params);
 	public HashMap<String, Object> getPwd(HashMap<String, Object> params);
-	public HashMap<String, Object> idCheck(HashMap<String, Object> params);
+	public HashMap<String, Object> idCheck(String userid);
 	public int insertMember(HashMap<String, Object> params);
-	public HashMap<String, Object> selectOne(String userid);
 	public int updateMember(HashMap<String, Object> params);
 	public int deleteMember(int u_idx);
 	public HashMap<String, Object> deletePwdCheck(String userid);
+	public HashMap<String, Object> selectOne(String userid);
 
 }

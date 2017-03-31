8148c7106a9de6c8a718096c52f3417adfe37ef1
student <student@M11024>
2017-03-31 17:03:34 +0900

    boardNotice limit, getCount

--- Jvb_tm/src/java/dao/boardnoticedao/IBoardNoticeDao.java
+++ Jvb_tm/src/java/dao/boardnoticedao/IBoardNoticeDao.java
@@ -8,6 +8,8 @@ public interface IBoardNoticeDao {
 	public int insertBoardNotice(HashMap<String, Object> params);
 	public int updateBoardNotice(HashMap<String, Object> params);
 	public int deleteBoardNotice(int b_idx);
-	public HashMap<String, Object> selectOne(int b_idx); //세부글 보기 
-	public List<HashMap<String, Object>> selectByCode(HashMap<String, Object> params); //게시판 별 목록
+	public HashMap<String, Object> selectOne(int b_idx);
+	public List<HashMap<String, Object>> selectByCode(HashMap<String, Object> params); 
+	public List<HashMap<String, Object>> selectNoticeLimit(HashMap<String, Object> params);
+	public int getCountNotice();
 }

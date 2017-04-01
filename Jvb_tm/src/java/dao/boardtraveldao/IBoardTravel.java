ea0f72d2cf4b386b32cabeadd09f32b54a0e6b30
김이슬 <@192.168.0.4>
2017-04-01 15:41:10 +0900

    김이슬 b_ 붙임

--- Jvb_tm/src/java/dao/boardtraveldao/IBoardTravel.java
+++ Jvb_tm/src/java/dao/boardtraveldao/IBoardTravel.java
@@ -11,8 +11,8 @@ public interface IBoardTravel {
 	public int deleteBoardTravel(int b_idx);
 	public HashMap<String, Object> selectTravelOne(int b_idx);
 	public List<HashMap<String, Object>> selectTravelByCode();
-	public List<HashMap<String, Object>> selectTravelByLoc_Category(String loc_category);
-	public List<HashMap<String, Object>> selectTravelBySub_Category(String sub_category);
+	public List<HashMap<String, Object>> selectTravelByLoc_Category(String b_loc_category);
+	public List<HashMap<String, Object>> selectTravelBySub_Category(String b_sub_category);
 	public BoardVo selectTravelByLimit(HashMap<String, Object> params); 
 	public int getCountTravel();
 	 

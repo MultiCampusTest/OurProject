900322180fb187fcd4e21d3756097e821fbf6e51
김이슬 <@192.168.0.4>
2017-04-01 15:20:06 +0900

    김이슬 notice, travel update

--- Jvb_tm/src/java/dao/boardnoticedao/IBoardNoticeDao.java
+++ Jvb_tm/src/java/dao/boardnoticedao/IBoardNoticeDao.java
@@ -2,15 +2,17 @@ package java.dao.boardnoticedao;
 
 import java.util.HashMap;
 import java.util.List;
+import java.vo.BoardVo;
 
 public interface IBoardNoticeDao {
 	
-	public int insertBoardNotice(HashMap<String, Object> params);
-	public int updateBoardNotice(HashMap<String, Object> params);
+	public int insertBoardNotice(BoardVo boardVo);
+	public int updateBoardNotice(BoardVo boardVo);
 	public int deleteBoardNotice(int b_idx);
-	public HashMap<String, Object> selectOne(int b_idx);
-	public List<HashMap<String, Object>> selectByCode(HashMap<String, Object> params); 
-	public List<HashMap<String, Object>> selectNoticeByLimit(HashMap<String, Object> params);
+	public BoardVo selectNoticeOne(int b_idx);
+	public BoardVo selectNoticeBySearch(HashMap<String, Object> params);
+	public BoardVo selectByCode(); 
+	public BoardVo selectNoticeByLimit(HashMap<String, Object> params);
 	public int getCountNotice();
 	
 }
--- Jvb_tm/src/java/dao/boardtraveldao/IBoardTravel.java
+++ Jvb_tm/src/java/dao/boardtraveldao/IBoardTravel.java
@@ -2,16 +2,18 @@ package java.dao.boardtraveldao;
 
 import java.util.HashMap;
 import java.util.List;
+import java.vo.BoardVo;
 
 public interface IBoardTravel {
 	
-	public int insertBoardTravel(HashMap<String, Object> params);
-	public int updateBoardTravel(HashMap<String, Object> params);
+	public int insertBoardTravel(BoardVo boardVo);
+	public int updateBoardTravel(BoardVo boardVo);
 	public int deleteBoardTravel(int b_idx);
-	public HashMap<String, Object> selectTravelOne(int b_idx); 
-	public List<HashMap<String, Object>> selectTravelByLoc_Category(HashMap<String, Object> params);
-	public List<HashMap<String, Object>> selectTravelBySub_Category(HashMap<String, Object> params);
-	public List<HashMap<String, Object>> selectByCode(HashMap<String, Object> params); 
+	public HashMap<String, Object> selectTravelOne(int b_idx);
+	public List<HashMap<String, Object>> selectTravelByCode();
+	public List<HashMap<String, Object>> selectTravelByLoc_Category(String loc_category);
+	public List<HashMap<String, Object>> selectTravelBySub_Category(String sub_category);
+	public BoardVo selectTravelByLimit(HashMap<String, Object> params); 
 	public int getCountTravel();
 	 
 }

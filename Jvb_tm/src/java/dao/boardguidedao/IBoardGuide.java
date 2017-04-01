6dd04f74a58434c09703a88d4189214655715219
bbureg <deawon1@naver.com>
2017-04-01 14:43:40 +0900

    수정

--- Jvb_tm/src/java/dao/boardguidedao/IBoardGuide.java
+++ Jvb_tm/src/java/dao/boardguidedao/IBoardGuide.java
@@ -8,8 +8,8 @@ public interface IBoardGuide {
 	public int insertBoardGuide(HashMap<String, Object> params);
 	public int updateBoardGuide(HashMap<String, Object> params);
 	public int deleteBoardGuide(int b_idx);
-	public HashMap<String, Object> selectOne(int b_idx); //세부글 보기 
-	public List<HashMap<String, Object>> selectByCode(HashMap<String, Object> params); //게시판 별 목록
+	public HashMap<String, Object> selectOne(int b_idx); 
+	public List<HashMap<String, Object>> selectByCode(HashMap<String, Object> params); 
 	public List<HashMap<String, Object>> selectByCategory(HashMap<String, Object> params);
 	public List<HashMap<String, Object>> selectGuideByLimit(HashMap<String, Object> params);
 	public int getCountGuide();

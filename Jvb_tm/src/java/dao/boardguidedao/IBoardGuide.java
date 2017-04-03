512196a6bc09cdaa642b6f276a124da2bf74cb46
MultiCampus1102 <dlrlftjs12@gmail.com>
2017-04-03 15:45:21 +0900

    이미지 내용 추가

--- Jvb_tm/src/java/dao/boardguidedao/IBoardGuide.java
+++ Jvb_tm/src/java/dao/boardguidedao/IBoardGuide.java
@@ -3,11 +3,17 @@ package java.dao.boardguidedao;
 import java.util.HashMap;
 import java.util.List;
 import java.vo.BoardVo;
+import java.vo.ContentsVo;
+import java.vo.ImageVo;
 
 public interface IBoardGuide {
 
 	public int insertBoardGuide(BoardVo boardVo);
+	public int insertImage(ImageVo imageVo);
+	public int insertContents(ContentsVo contens);
 	public int updateBoardGuide(BoardVo boardVo);
+	public int updateImage(ImageVo imageVo);
+	public int updateContents(ContentsVo contents);
 	public int deleteBoardGuide(int b_idx);
 	public HashMap<String, Object> selectGuideOne(int b_idx); 
 	public List<HashMap<String, Object>> selectGuideByCode(); 
--- Jvb_tm/src/java/dao/boardreviewdao/IBoardReviewDao.java
+++ Jvb_tm/src/java/dao/boardreviewdao/IBoardReviewDao.java
@@ -3,11 +3,17 @@ package java.dao.boardreviewdao;
 import java.util.HashMap;
 import java.util.List;
 import java.vo.BoardVo;
+import java.vo.ContentsVo;
+import java.vo.ImageVo;
 
 public interface IBoardReviewDao {
 	
 	public int insertReviewBoard(BoardVo boardVo);
+	public int insertImage(ImageVo imageVo);
+	public int insertContents(ContentsVo contens);
 	public int updateReviewBoard(BoardVo boardVo);
+	public int updateImage(ImageVo imageVo);
+	public int updateContents(ContentsVo contents);
 	public int deleteReviewBoard(int b_idx);
 	public HashMap<String, Object> selectReviewOne(int b_idx); //세부글 보기 
 	public List<HashMap<String, Object>> selectReivewByCode(); //게시판 별 목록

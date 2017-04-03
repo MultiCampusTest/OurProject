806d6f0f2d62e6bd4201462d24f97702d7d8c87c
jiamorena <seokhn09@gmail.com>
2017-04-03 16:32:39 +0900

    up

--- Jvb_tm/src/java/dao/memberdao/IMemberDao.java
+++ Jvb_tm/src/java/dao/memberdao/IMemberDao.java
@@ -1,5 +1,7 @@
 package java.dao.memberdao;
 
+import java.util.HashMap;
+import java.vo.ImageVo;
 import java.vo.MemberVo;
 
 public interface IMemberDao {
@@ -8,10 +10,10 @@ public interface IMemberDao {
 	public MemberVo getPwd(MemberVo memberVo);
 	public MemberVo idCheck(String userid);
 	public int insertMember(MemberVo memberVo);
+	public int insertImage(ImageVo imageVo);
 	public int updateMember(MemberVo memberVo);
+	public int updateImage(ImageVo imageVo);
 	public int deleteMember(int u_idx);
 	public MemberVo deletePwdCheck(String userid);
-	public MemberVo selectOne(String userid);
-	
-	
+	public HashMap<String, Object> selectOne(String userid);
 }

9dc15f9be75ea8f0025407323dbe61d61250a594
김이슬 <@192.168.0.9>
2017-04-03 16:08:37 +0900

    notice

--- Jvb_tm/src/java/dao/boardnoticedao/IBoardNoticeDao.java
+++ Jvb_tm/src/java/dao/boardnoticedao/IBoardNoticeDao.java
@@ -3,13 +3,18 @@ package java.dao.boardnoticedao;
 import java.util.HashMap;
 import java.util.List;
 import java.vo.BoardVo;
+import java.vo.ContentsVo;
 
 public interface IBoardNoticeDao {
 	
 	public int insertBoardNotice(BoardVo boardVo);
+	public int insertContentsNotice(ContentsVo contentsVo);
 	public int updateBoardNotice(BoardVo boardVo);
+	public int updaetContentsNotice(ContentsVo contentsVo);
 	public int deleteBoardNotice(int b_idx);
+	public int deleteContentsNotice(int b_idx);
 	public BoardVo selectNoticeOne(int b_idx);
+	public ContentsVo selectContentsNoticeOne(int b_idx);
 	public BoardVo selectNoticeBySearch(HashMap<String, Object> params);
 	public BoardVo selectByCode(); 
 	public BoardVo selectNoticeByLimit(HashMap<String, Object> params);
--- Jvb_tm/src/java/dao/boardtraveldao/IBoardTravel.java
+++ Jvb_tm/src/java/dao/boardtraveldao/IBoardTravel.java
@@ -3,13 +3,21 @@ package java.dao.boardtraveldao;
 import java.util.HashMap;
 import java.util.List;
 import java.vo.BoardVo;
+import java.vo.ContentsVo;
+import java.vo.ImageVo;
 
 public interface IBoardTravel {
 	
 	public int insertBoardTravel(BoardVo boardVo);
+	public int insertContentsTravel(ContentsVo contentsVo);
+	public int insertImageTravel(ImageVo imageVo);
 	public int updateBoardTravel(BoardVo boardVo);
+	public int updateImageTravel(ImageVo imageVo);
 	public int deleteBoardTravel(int b_idx);
+	public int deleteContentsTravel(int b_idx);
+	public int deleteImageTravel(int img_idx);
 	public HashMap<String, Object> selectTravelOne(int b_idx);
+	public ContentsVo selectContentsTravelOne(int b_idx);
 	public List<HashMap<String, Object>> selectTravelByCode();
 	public List<HashMap<String, Object>> selectTravelByLoc_Category(String b_loc_category);
 	public List<HashMap<String, Object>> selectTravelBySub_Category(String b_sub_category);

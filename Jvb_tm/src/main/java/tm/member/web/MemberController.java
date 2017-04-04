fdce563ada8d228773ad45d18f057b6e58335050
jiamorena <seokhn09@gmail.com>
2017-04-04 16:58:32 +0900

    up

--- Jvb_tm/src/main/java/tm/member/web/MemberController.java
+++ Jvb_tm/src/main/java/tm/member/web/MemberController.java
@@ -3,24 +3,10 @@ package tm.member.web;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/join.do")
-public class MemberController extends HttpServlet {
-
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-	}
+public class MemberController {
 
 }

ab524caeae6bc470a7dbb01dabda0fd0a58187cf
bbureg <deawon1@naver.com>
2017-04-05 15:03:06 +0900

    vo 2차 수정본

---
+++ Jvb_tm/src/main/java/tm/commons/vo/ImageVo.java
@@ -0,0 +1,48 @@
+package tm.commons.vo;
+
+public class ImageVo {
+
+   private int img_idx;
+   private String img_phy_name;
+   private String img_ori_name;
+   private int img_code;
+   private String img_path;
+   public int getImg_idx() {
+      return img_idx;
+   }
+   public void setImg_idx(int img_idx) {
+      this.img_idx = img_idx;
+   }
+   public String getImg_phy_name() {
+      return img_phy_name;
+   }
+   public void setImg_phy_name(String img_phy_name) {
+      this.img_phy_name = img_phy_name;
+   }
+   public String getImg_ori_name() {
+      return img_ori_name;
+   }
+   public void setImg_ori_name(String img_ori_name) {
+      this.img_ori_name = img_ori_name;
+   }
+   public int getImg_code() {
+      return img_code;
+   }
+   public void setImg_code(int img_code) {
+      this.img_code = img_code;
+   }
+   public String getImg_path() {
+      return img_path;
+   }
+   public void setImg_path(String img_path) {
+      this.img_path = img_path;
+   }
+   @Override
+   public String toString() {
+      return "ImageVo [img_idx=" + img_idx + ", img_phy_name=" + img_phy_name
+            + ", img_ori_name=" + img_ori_name + ", img_code=" + img_code
+            + ", img_path=" + img_path + "]";
+   }
+}
+   
+

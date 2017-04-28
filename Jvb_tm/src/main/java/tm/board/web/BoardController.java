705d5e40aa81393b29a5526db0c32e492153e4a7
student <student@M9013>
2017-04-28 15:41:46 +0900

    scrollreveal js 추가, boardController에 경로 추가 - 리나

--- Jvb_tm/src/main/java/tm/board/web/BoardController.java
+++ Jvb_tm/src/main/java/tm/board/web/BoardController.java
@@ -7,52 +7,97 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BoardController {
 
 	@RequestMapping("main.do")
-	public String main(){
+	public String main() {
 		return "middle";
 	}
-	
+
 	@RequestMapping("myPage.do")
-	public String maindo(){
+	public String maindo() {
 		return "myPage";
 	}
-	
-   @RequestMapping("noticeList.do")
-   public String noticeList(){
-	   return "noticeList";
-   }
-   
-   @RequestMapping("noticeView.do")
-   public String noticeView(){
-	   return "noticeView";
-   }
-   
-   @RequestMapping("noticeWriteForm.do")
-   public String noticeWriteForm(){
-	   return "noticeWriteForm";
-   }
-   
-   @RequestMapping("noticeUpdateForm.do")
-   public String noticeUpdateForm(){
-	   return "noticeUpdateForm";
-   }
-   
-   @RequestMapping("travelList.do")
-   public String travelList(){
-	   return "travelList";
-   }
-   
-   @RequestMapping("guideWriteForm.do")
-   public String guideWriteForm(){
-	   return "guideWriteForm";
-   }
-   
-   @RequestMapping("travelWriteForm.do")
-   public String travelWriteForm(){
-	   return "travelWriteForm";
-   }
-   
-   @RequestMapping("reviewWriteForm.do")
-   public String reviewWriteForm(){
-	   return "reviewWriteForm";
-   }
+
+	// notice
+	@RequestMapping("noticeList.do")
+	public String noticeList() {
+		return "noticeList";
+	}
+
+	@RequestMapping("noticeView.do")
+	public String noticeView() {
+		return "noticeView";
+	}
+
+	@RequestMapping("noticeWriteForm.do")
+	public String noticeWriteForm() {
+		return "noticeWriteForm";
+	}
+
+	@RequestMapping("noticeModifyForm.do")
+	public String noticeModifyForm() {
+		return "noticeModifyForm";
+	}
+
+	// travel
+	@RequestMapping("travelList.do")
+	public String travelList() {
+		return "travelList";
+	}
+
+	@RequestMapping("travelWriteForm.do")
+	public String travelWriteForm() {
+		return "travelWriteForm";
+	}
+
+	@RequestMapping("travelView.do")
+	public String travelView() {
+		return "travelList";
+	}
+
+	@RequestMapping("travelModifyForm.do")
+	public String travelModifyForm() {
+		return "travelList";
+	}
+
+	// guide
+	@RequestMapping("guideWriteForm.do")
+	public String guideWriteForm() {
+		return "guideWriteForm";
+	}
+
+	@RequestMapping("guideList.do")
+	public String guideList() {
+		return "guideList";
+	}
+
+	@RequestMapping("guideModifyForm.do")
+	public String guideModifyForm() {
+		return "guideModifyForm";
+	}
+
+	@RequestMapping("guideView.do")
+	public String guideView() {
+		return "guideView";
+	}
+
+	// review
+	@RequestMapping("reviewWriteForm.do")
+	public String reviewWriteForm() {
+		return "reviewWriteForm";
+	}
+
+	@RequestMapping("reviewList.do")
+	public String reviewList() {
+		return "reviewList";
+	}
+
+	@RequestMapping("reviewView.do")
+	public String reviewView() {
+		return "reviewView";
+	}
+
+	@RequestMapping("reviewModifyForm.do")
+	public String reviewModifyForm() {
+		return "reviewModifyForm";
+	}
+
 }

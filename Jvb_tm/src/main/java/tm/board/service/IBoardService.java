75c853b87f71cc915e1a28001f98f752b21ed1ea
MultiCampus1102 <dlrlftjs12@gmail.com>
2017-05-02 14:38:04 +0900

    길선 - IBoardService 올림

---
+++ Jvb_tm/src/main/java/tm/board/service/IBoardService.java
@@ -0,0 +1,47 @@
+package tm.board.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import tm.board.vo.BoardVo;
+import tm.board.vo.CommentsVo;
+import tm.board.vo.ContentsVo;
+import tm.image.vo.ImageVo;
+
+public interface IBoardService {
+	//게시글 작성
+	public void writeNotice(BoardVo board, ContentsVo contents);
+	public void writeGuide(BoardVo board, ImageVo image, ContentsVo contents);
+	public void writeTravel(BoardVo board, ImageVo image, ContentsVo contens);
+	public void wirteReview(BoardVo board, ImageVo imge, ContentsVo contents);
+	public void writeComments(CommentsVo comments);
+	
+	//게시글 수정
+	public void modifyNotice(BoardVo board, ContentsVo contents);
+	public void modifyGuide(BoardVo board, ImageVo image, ContentsVo contents);
+	public void modifyTravel(BoardVo board, ImageVo image, ContentsVo contents);
+	public void modifyReview(BoardVo board, ImageVo image, ContentsVo contents);
+	public void modifyComments(CommentsVo comments);
+	
+	//게시글 삭제
+	public boolean boardDelete(int b_idx);
+	
+	//각 게시글 한개
+	public HashMap<String, Object> selectOneNotice(int b_idx, int ct_idx);
+	public HashMap<String, Object> selectOneGuide(int b_idx, int img_idx, int ct_idx);
+	public HashMap<String, Object> selectOneTrabel(int b_idx, int img_idx, int ct_idx);
+	public HashMap<String, Object> selectOneReview(int b_idx, int img_idx, int ct_idx);
+	
+	//각 게시글 댓글 리스트 출력
+	public List<HashMap<String, Object>> selectComments(int b_idx);
+	
+	//게시판별 리스트 출력
+	public List<HashMap<String, Object>> selectAllNotice(int b_code, int b_idx, int page);
+	public List<HashMap<String, Object>> selectAllGuide(int b_code, int b_idx, int page);
+	public List<HashMap<String, Object>> selectAllTravel(int b_code, int b_idx, int page);
+	public List<HashMap<String, Object>> selectAllReview(int b_code, int b_idx, int page);
+	
+	//게시글 조회
+	public HashMap<String, Object> readBoard(int b_idx);
+
+}

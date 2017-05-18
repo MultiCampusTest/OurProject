66f93667308db24a14c2373f0cf1e415f29758a8
KYS <KYS@DESKTOP-1MUC501>
2017-05-18 17:38:16 +0900

    김이슬 이미지 커밋

--- Jvb_tm/src/main/java/tm/image/dao/IImageDao.java
+++ Jvb_tm/src/main/java/tm/image/dao/IImageDao.java
@@ -30,5 +30,5 @@ public interface IImageDao {
    public int updateReviewImage(ImageVo imageVo);
    
    
-   
+
 }
--- Jvb_tm/src/main/java/tm/image/service/ImageService.java
+++ Jvb_tm/src/main/java/tm/image/service/ImageService.java
@@ -135,6 +135,7 @@ public class ImageService implements IImageService {
       return image;
    }
 
+ 
    
    
    @Override

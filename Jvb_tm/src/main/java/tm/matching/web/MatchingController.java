2250e00bcf86a4af0ccd389e0b76d9bf7b319e6e
Amalia <Amalia@AmaliaPC>
2017-06-23 16:40:38 +0900

    리나 - 수정

--- Jvb_tm/src/main/java/tm/matching/web/MatchingController.java
+++ Jvb_tm/src/main/java/tm/matching/web/MatchingController.java
@@ -21,121 +21,114 @@ import tm.message.vo.MessageVo;
 
 @Controller
 public class MatchingController {
-   
-   @Autowired
-   private MatchingService matchingService;
-   
-//   @RequestMapping("matchingList.do")
-//   public ModelAndView matchingList(String mch_g_userid){
-//      String black="black";
-//      ModelAndView mav=new ModelAndView();
-//      mav.addAllObjects(matchingService.matchingList(black));
-//      mav.setViewName("member/my_page");
-//      return mav;
-//   }
-   
-   
-   
-   
-   @RequestMapping("guideMatching.do")
-   public ModelAndView guideMatching(HttpSession session){
-      String userid = (String) session.getAttribute("userid");
-      System.out.println(session.getAttribute("userid"));
-      
-      String url="g_matching";
-      HashMap<String, Object> params=new HashMap<>();
-      params.put("userid", userid);
-      params.put("url", url);
-      
-      ModelAndView mav=new ModelAndView();
-      mav.addObject("type", "g_matching");
-      mav.addAllObjects(matchingService.matchingList(userid));
-      mav.addAllObjects(params);
-      mav.setViewName("member/my_page");
-      return mav;
-   }
-   
-   @RequestMapping(value="androidMatchingList.do", method=RequestMethod.GET)
-   public @ResponseBody HashMap<String, Object> and_matchingList(String userid){
-      System.out.println("안드로이드 매칭 리스트");
-//      HashMap<String, Object> response=new HashMap<>();
-//      Gson gson = new Gson(); 
-//      String str = gson.toJson(matchingService.matchingList(userid));
-//      response.put("matchingListStr", str);
-//      return response;
-      return matchingService.matchingList(userid);
-   }
-   
-   
-   @RequestMapping("travelMatching.do")
-   public ModelAndView travelMatching(HttpSession session){
-      String userid = (String) session.getAttribute("userid");
-      System.out.println(session.getAttribute("userid"));
-      
-      String url="t_matching";
-      HashMap<String, Object> params=new HashMap<>();
-      params.put("userid", userid);
-      params.put("url", url);
-      
-      ModelAndView mav=new ModelAndView();
-      mav.addObject("type", "t_matching");
-      mav.addAllObjects(matchingService.matchingList(userid));
-      mav.addAllObjects(params);
-      mav.setViewName("member/my_page");
-      
-      System.out.println(matchingService.matchingList(userid));
-      return mav;
-   }
-   
-   
-      
-   
-   
-   
-   
-//   @RequestMapping(value="matchingHolding.do", method=RequestMethod.POST)
-//   public @ResponseBody HashMap<String, Object> matchingHolding(HttpSession session, int b_idx, String mch_g_userid){
-//       String mch_t_userid=(String)session.getAttribute("userid");
-//         boolean result=matchingService.matchingModify(b_idx, mch_g_userid, 2);
-//         if(result==true){
-//              return matchingService.matchingList(mch_t_userid);
-//         }
-//         else
-//            return matchingService.matchingList(mch_t_userid);
-//   }
-   
-    @RequestMapping(value="matchingSuccess.do", method=RequestMethod.POST)
-      public @ResponseBody HashMap<String, Object> matchingSuccess(HttpSession session, int b_idx, String mch_g_userid){
-       System.out.println("야");
-         String mch_t_userid=(String)session.getAttribute("userid");
-         matchingService.matchingModify(b_idx, mch_g_userid, 1);
-        
-         return matchingService.matchingList(mch_t_userid);
-         
-      }
-    
-    @RequestMapping(value="androidMatchingSuccess.do", method=RequestMethod.GET)
-    public @ResponseBody HashMap<String, Object> androidMatchingSuccess(String b_idx, String mch_g_userid, String mch_t_userid){
-       System.out.println("안드로이드 매칭 수락 요청");
-       int num = Integer.parseInt(b_idx);
-         matchingService.matchingModify(num, mch_g_userid, 1);
-         return matchingService.matchingList(mch_t_userid);
-         
-     } 
-    
-    
-    @RequestMapping(value="matchingReject.do",  method=RequestMethod.POST)
-    public @ResponseBody HashMap<String, Object> matchingReject(HttpSession session, int b_idx, String mch_g_userid){
-       System.out.println("여기 오니");
-       String mch_t_userid=(String)session.getAttribute("userid");
-//         HashMap<String, Object> params=new HashMap<>();
-         matchingService.matchingModify(b_idx, mch_g_userid, 3);
-//         params.put("mch_List_Reload", matchingService.matchingList(mch_t_userid));
-         System.out.println( "외냐");
-         
-        return matchingService.matchingList(mch_t_userid);
-    }
-    
 
+	@Autowired
+	private MatchingService matchingService;
+
+	// @RequestMapping("matchingList.do")
+	// public ModelAndView matchingList(String mch_g_userid){
+	// String black="black";
+	// ModelAndView mav=new ModelAndView();
+	// mav.addAllObjects(matchingService.matchingList(black));
+	// mav.setViewName("member/my_page");
+	// return mav;
+	// }
+
+	@RequestMapping("guideMatching.do")
+	public ModelAndView guideMatching(HttpSession session) {
+		String userid = (String) session.getAttribute("userid");
+		System.out.println(session.getAttribute("userid"));
+
+		String url = "g_matching";
+		HashMap<String, Object> params = new HashMap<>();
+		params.put("userid", userid);
+		params.put("url", url);
+
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("type", "g_matching");
+		mav.addAllObjects(matchingService.matchingList(userid));
+		mav.addAllObjects(params);
+		mav.setViewName("member/my_page");
+		return mav;
+	}
+
+	@RequestMapping(value = "androidMatchingList.do", method = RequestMethod.GET)
+	public @ResponseBody HashMap<String, Object> and_matchingList(String userid) {
+		System.out.println("안드로이드 매칭 리스트");
+		// HashMap<String, Object> response=new HashMap<>();
+		// Gson gson = new Gson();
+		// String str = gson.toJson(matchingService.matchingList(userid));
+		// response.put("matchingListStr", str);
+		// return response;
+		return matchingService.matchingList(userid);
+	}
+
+	@RequestMapping("travelMatching.do")
+	public ModelAndView travelMatching(HttpSession session) {
+		String userid = (String) session.getAttribute("userid");
+		System.out.println(session.getAttribute("userid"));
+
+		String url = "t_matching";
+		HashMap<String, Object> params = new HashMap<>();
+		params.put("userid", userid);
+		params.put("url", url);
+
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("type", "t_matching");
+		mav.addAllObjects(matchingService.matchingList(userid));
+		mav.addAllObjects(params);
+		mav.setViewName("member/my_page");
+
+		System.out.println(matchingService.matchingList(userid));
+		return mav;
+	}
+
+	// @RequestMapping(value="matchingHolding.do", method=RequestMethod.POST)
+	// public @ResponseBody HashMap<String, Object> matchingHolding(HttpSession
+	// session, int b_idx, String mch_g_userid){
+	// String mch_t_userid=(String)session.getAttribute("userid");
+	// boolean result=matchingService.matchingModify(b_idx, mch_g_userid, 2);
+	// if(result==true){
+	// return matchingService.matchingList(mch_t_userid);
+	// }
+	// else
+	// return matchingService.matchingList(mch_t_userid);
+	// }
+
+	@RequestMapping(value = "matchingSuccess.do", method = RequestMethod.POST)
+	public @ResponseBody HashMap<String, Object> matchingSuccess(HttpSession session, int b_idx, String mch_g_userid) {
+		System.out.println("야");
+		String mch_t_userid = (String) session.getAttribute("userid");
+		matchingService.matchingModify(b_idx, mch_g_userid, 1);
+
+		return matchingService.matchingList(mch_t_userid);
+
+	}
+
+	@RequestMapping(value = "androidMatchingSuccess.do", method = RequestMethod.GET)
+	public @ResponseBody HashMap<String, Object> androidMatchingSuccess(String b_idx, String mch_g_userid,
+			String mch_t_userid) {
+		System.out.println("안드로이드 매칭 수락 요청");
+		
+		String[] str = b_idx.split(".");
+		int num = Integer.parseInt(str[0]);
+		
+		matchingService.matchingModify(num, mch_g_userid, 1);
+		return matchingService.matchingList(mch_t_userid);
+
+	}
+
+	@RequestMapping(value = "matchingReject.do", method = RequestMethod.POST)
+	public @ResponseBody HashMap<String, Object> matchingReject(HttpSession session, int b_idx, String mch_g_userid) {
+		System.out.println("여기 오니");
+		String mch_t_userid = (String) session.getAttribute("userid");
+		// HashMap<String, Object> params=new HashMap<>();
+		matchingService.matchingModify(b_idx, mch_g_userid, 3);
+		// params.put("mch_List_Reload",
+		// matchingService.matchingList(mch_t_userid));
+		System.out.println("외냐");
+
+		return matchingService.matchingList(mch_t_userid);
+	}
 
 }

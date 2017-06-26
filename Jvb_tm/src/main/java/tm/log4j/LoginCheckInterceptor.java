375723b8e5f9dfd9a918c34323a1ee8fc519bf40
Amalia <Amalia@AmaliaPC>
2017-06-26 09:46:58 +0900

    리나 - 인터셉터, 로그 수정

--- Jvb_tm/src/main/java/tm/log4j/LoginCheckInterceptor.java
+++ Jvb_tm/src/main/java/tm/log4j/LoginCheckInterceptor.java
@@ -24,7 +24,7 @@ public class LoginCheckInterceptor extends HandlerInterceptorAdapter{
 //		return super.preHandle(request, response, handler);
 		if(request.getSession().getAttribute("userid") == null)
 		{	
-			response.sendRedirect("loginRequest.do");
+			response.sendRedirect("loginProc.do");
 			return false;
 		}
 		else
